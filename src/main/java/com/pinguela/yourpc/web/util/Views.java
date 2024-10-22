8fb9ca2e2408f3cbc8253a766b78555db149d9f4
Alexandre <pereb27@gmail.com>
2024-10-22 22:24:23 +0200

    Update constants

--- src/main/java/com/pinguela/yourpc/web/util/Views.java
+++ src/main/java/com/pinguela/yourpc/web/util/Views.java
@@ -2,8 +2,8 @@ package com.pinguela.yourpc.web.util;
 
 public final class Views {
 	
-	public static final String HOME = "/YourPCWeb/index.jsp";
-	public static final String LOGIN = "/YourPCWeb/login.jsp";
+	public static final String HOME = "/index.jsp";
+	public static final String LOGIN = "/login.jsp";
 	
 
 }
