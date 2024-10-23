9d5454c39203d540caadac618e592fb944fb7d19
Alexandre <pereb27@gmail.com>
2024-10-23 22:24:03 +0200

    Add methods to handle cookies

--- src/main/java/com/pinguela/yourpc/web/util/CookieManager.java
+++ src/main/java/com/pinguela/yourpc/web/util/CookieManager.java
@@ -1,13 +1,34 @@
 package com.pinguela.yourpc.web.util;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class CookieManager {
 	
-	public static void getCookie() {
-		
+	public static Cookie getCookie(HttpServletRequest request, String name) {
+		Cookie[] cookies = request.getCookies();
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equals(name)) {
+				return cookie;
+			}
+		}
+		return null;
 	}
 	
-	public static void setCookie() {
-		
+	public static String getValue(HttpServletRequest request, String name) {
+		return getCookie(request, name).getValue();
+	}
+	
+	public static void addCookie(HttpServletResponse response, String path, String name, String value, int ttl) {
+		Cookie cookie = new Cookie(name, value);
+		cookie.setMaxAge(ttl);
+		cookie.setPath(path);
+		response.addCookie(cookie);
+	}
+	
+	public static void removeCookie(HttpServletResponse response, String path, String name) {
+		addCookie(response, path, name, "", 0);
 	}
 
 }
