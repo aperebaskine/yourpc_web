30e7a21c8eed168717a50b181e2a6e364792dff5
Alexandre <pereb27@gmail.com>
2024-10-24 22:21:06 +0200

    Split authentication filter

--- src/main/java/com/pinguela/yourpc/web/filter/CustomerAuthenticationFilter.java
+++ src/main/java/com/pinguela/yourpc/web/filter/CustomerAuthenticationFilter.java
@@ -1,7 +1,7 @@
 package com.pinguela.yourpc.web.filter;
 
 import com.pinguela.yourpc.model.Customer;
-import com.pinguela.yourpc.web.util.Views;
+import com.pinguela.yourpc.web.constants.Views;
 
 @SuppressWarnings("serial")
 public class CustomerAuthenticationFilter
--- src/main/java/com/pinguela/yourpc/web/filter/EmployeeAuthenticationFilter.java
+++ src/main/java/com/pinguela/yourpc/web/filter/EmployeeAuthenticationFilter.java
@@ -1,7 +1,7 @@
 package com.pinguela.yourpc.web.filter;
 
 import com.pinguela.yourpc.model.Employee;
-import com.pinguela.yourpc.web.util.Views;
+import com.pinguela.yourpc.web.constants.Views;
 
 @SuppressWarnings("serial")
 public class EmployeeAuthenticationFilter 
