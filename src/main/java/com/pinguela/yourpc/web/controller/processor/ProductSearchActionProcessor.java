1cfde2714ccd68d0053351673ebc6014b4c205fc
Alexandre Perebaskine <pereb27@gmail.com>
2024-11-02 01:34:41 +0100

    Remove service locator call

--- src/main/java/com/pinguela/yourpc/web/controller/processor/ProductSearchActionProcessor.java
+++ src/main/java/com/pinguela/yourpc/web/controller/processor/ProductSearchActionProcessor.java
@@ -9,32 +9,34 @@ import com.pinguela.yourpc.model.Product;
 import com.pinguela.yourpc.model.ProductCriteria;
 import com.pinguela.yourpc.model.Results;
 import com.pinguela.yourpc.service.ProductService;
-import com.pinguela.yourpc.web.constants.RouteMethod;
+import com.pinguela.yourpc.service.impl.ProductServiceImpl;
+import com.pinguela.yourpc.web.constants.Actions;
 import com.pinguela.yourpc.web.controller.ProductServlet;
 import com.pinguela.yourpc.web.model.Route;
-import com.pinguela.yourpc.web.util.ServiceManager;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@ActionProcessor(action = Actions.RESULTS, servlets = ProductServlet.class)
 public class ProductSearchActionProcessor 
 extends AbstractActionProcessor {
+	
+	private ProductService service;
 		
 	public ProductSearchActionProcessor() {
-		super((String) null, ProductServlet.class);
+		service = new ProductServiceImpl();
 	}
 
 	@Override
-	public void doAction(HttpServletRequest request, HttpServletResponse response, Route route)
+	public void processAction(HttpServletRequest request, HttpServletResponse response, Route route)
 			throws ServletException, IOException, YPCException {
 		ProductCriteria criteria = new ProductCriteria();
 		criteria.setName(request.getParameter(NAME));
 
-		Results<Product> results = ServiceManager.get(ProductService.class).findBy(criteria, 1, 10);
+		Results<Product> results = service.findBy(criteria, 1, 10);
 		request.setAttribute("results", results);
 		route.setTargetView("/product/product-results-view.jsp");
-		route.setRouteMethod(RouteMethod.FORWARD);
 	}
 
 }
