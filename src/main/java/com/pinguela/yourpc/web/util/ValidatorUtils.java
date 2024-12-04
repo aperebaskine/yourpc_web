c7666e3720f5dd467b3002164e68a0aa30ee2f46
Alexandre <pereb27@gmail.com>
2024-12-04 20:07:52 +0100

    Check email with GenericValidator

--- src/main/java/com/pinguela/yourpc/web/util/ValidatorUtils.java
+++ src/main/java/com/pinguela/yourpc/web/util/ValidatorUtils.java
@@ -25,10 +25,6 @@ public class ValidatorUtils {
 	private static final int PASSWORD_MAX_LENGTH = 20;
 
 	private static final Pattern SPECIAL_CHARACTER_REGEX = Pattern.compile("[^\\w\\d]");
-	public static final Pattern EMAIL_REGEX = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*"
-			+ "|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")"
-			+ "@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}"
-			+ "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
 
 	public static String getParameter(HttpServletRequest request, String parameterName, boolean isRequired) {
 		String parameterValue = request.getParameter(parameterName);
@@ -123,7 +119,7 @@ public class ValidatorUtils {
 	}
 	
 	public static boolean isValidEmail(HttpServletRequest request, String parameterName, String email) {
-		if (!EMAIL_REGEX.matcher(email).matches()) {
+		if (!GenericValidator.isEmail(email)) {
 			logFieldError(request, parameterName, ErrorCodes.INVALID_FORMAT);
 			return false;
 		}
